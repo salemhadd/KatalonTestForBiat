302e63cc082bf451e070c54d2e1d840be4dc2f6a
hp <hp@DESKTOP-SSHD5LR>
2021-07-23 23:00:30 +1300

    Last of LAst

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>13a08386-22c3-4a51-ad40-b0f2a881a905</testSuiteGuid>
+   <testCaseLink>
+      <guid>69f034de-0038-449f-8e6f-4d8309084e0a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/FirstTestCase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
